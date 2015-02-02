de07718ab67c05ed75579a4fb6dc8ea9d0a07695
Žiga <zigomir@gmail.com>
2015-02-02 21:57:14 +0100

    Update Head.js
    Fixes "Warning: Unknown DOM property http-equiv. Did you mean httpEquiv?"

--- components/Head.js
+++ components/Head.js
@@ -17,7 +17,7 @@ var Head = React.createClass({
       <head>
         <meta charSet="utf-8" />
         <title>{this.props.title}</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="mobile-web-app-capable" content="yes"/>
