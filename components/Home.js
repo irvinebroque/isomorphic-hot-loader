ecba2762317c9400744bbd54ddaf2277e3632b98
Brendan Irvine-Broque <brendanib@gmail.com>
2015-02-02 21:34:07 -0800

    isomorphic fetch working

--- components/Home.js
+++ components/Home.js
@@ -3,10 +3,27 @@
 var React = require('react');
 var Title = require('react-document-title');
 var Link = require('react-router').Link;
+require('isomorphic-fetch');
 
 var Home = React.createClass({
 
+  componentWillMount: function() {
+    fetch('//offline-news-api.herokuapp.com/stories')
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(stories) {
+        this.setState({stories: stories});
+      }.bind(this));
+  },
+
   render: function() {
+
+    console.log(this.state);
+
     return (
       <Title title='Home'>
         <div>
